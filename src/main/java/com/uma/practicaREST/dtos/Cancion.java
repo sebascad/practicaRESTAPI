9a29e5557eb34647452996c1fa29ac203a40a2aa
Sebas <sebasmesacadiz5@gmail.com>
2026-03-18 17:12:08 +0100

    Implemented Lombok annotations in Cancion class

--- src/main/java/com/uma/practicaREST/dtos/Cancion.java
+++ src/main/java/com/uma/practicaREST/dtos/Cancion.java
@@ -1,47 +1,12 @@
 package com.uma.practicaREST.dtos;
 
+import lombok.*;
+
+//Hacemos uso de la libreria Lombok para ayudarnos a reducir el "boilerplate" caracteristico de Java
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter
 public class Cancion {
     private Long id;
     private String titulo;
     private String cantante;
     private int anio;
-
-    public Cancion(int anio, String cantante, String titulo, Long id) {
-        this.anio = anio;
-        this.cantante = cantante;
-        this.titulo = titulo;
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getCantante() {
-        return cantante;
-    }
-
-    public void setCantante(String cantante) {
-        this.cantante = cantante;
-    }
-
-    public int getAnio() {
-        return anio;
-    }
-
-    public void setAnio(int anio) {
-        this.anio = anio;
-    }
 }
